559759fc355514527b7876c7f571008edbe38da7
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2026-04-15 10:26:13 +0300

    configure prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-
 import './App.css'
 
 function App() {
-
   return (
     <>
       <h1>Husky</h1>
-       <strong>npm install --save-dev husky</strong>
+      <strong>npm install --save-dev husky</strong>
 
-        <div>installs husky as a development dependency</div>
+      <div>installs husky as a development dependency</div>
 
-       <strong>npx husky init</strong>
-       <ul>automatically initializes husky</ul>
-       <ul>creates the .husky folder</ul>
-
+      <strong>npx husky init</strong>
+      <ul>automatically initializes husky</ul>
+      <ul>creates the .husky folder</ul>
     </>
   )
 }
