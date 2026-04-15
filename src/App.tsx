db266be91974ed38eecc7affa3dfaef9c7349f89
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2026-04-15 10:13:37 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Husky</h1>
+       <strong>npm install --save-dev husky</strong>
+
+        <div>installs husky as a development dependency</div>
+
+       <strong>npx husky init</strong>
+       <ul>automatically initializes husky</ul>
+       <ul>creates the .husky folder</ul>
+
+    </>
+  )
+}
+
+export default App
