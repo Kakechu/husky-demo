44d37398ec95f8a5aba5db08aa751cac03668bc9
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2026-04-15 13:54:10 +0300

    add pre-push file

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,10 @@ function App() {
     <>
       <h1>Husky</h1>
       <strong>npm install --save-dev husky</strong>
-
       <div>installs husky as a development dependency</div>
-
       <strong>npx husky init</strong>
-      <ul>automatically initializes husky</ul>
-      <ul>creates the .husky folder</ul>
+      <div>automatically initializes husky</div>
+      <div>creates the .husky folder</div>
     </>
   )
 }
--- src/add.test.ts
+++ src/add.test.ts
@@ -3,6 +3,6 @@ import { add } from './add'
 
 describe('add', () => {
   it('adds two numbers', () => {
-    expect(add(1, 2)).toBe(3)
+    expect(add(1, 2)).toBe(4)
   })
 })
